5fbe7ffd38366d1c41216d6d8f8b842b0ad40e33
PerryMartin <perry.martin@ocsbstudent.ca>
2015-11-03 13:20:19 -0500

    Hello

---
+++ Grading/GradingForm.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Grading
+{
+    partial class frmGrading
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.btnMark = new System.Windows.Forms.Button();
+            this.txtMark = new System.Windows.Forms.TextBox();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(13, 13);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(107, 13);
+            this.lblInfo.TabIndex = 0;
+            this.lblInfo.Text = "Place you mark here.";
+            // 
+            // btnMark
+            // 
+            this.btnMark.Location = new System.Drawing.Point(16, 55);
+            this.btnMark.Name = "btnMark";
+            this.btnMark.Size = new System.Drawing.Size(75, 23);
+            this.btnMark.TabIndex = 1;
+            this.btnMark.Text = "Find Mark";
+            this.btnMark.UseVisualStyleBackColor = true;
+            this.btnMark.Click += new System.EventHandler(this.btnMark_Click);
+            // 
+            // txtMark
+            // 
+            this.txtMark.Location = new System.Drawing.Point(12, 29);
+            this.txtMark.Name = "txtMark";
+            this.txtMark.Size = new System.Drawing.Size(100, 20);
+            this.txtMark.TabIndex = 2;
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Location = new System.Drawing.Point(130, 60);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(111, 13);
+            this.lblAnswer.TabIndex = 3;
+            this.lblAnswer.Text = "Your percentage is: ...";
+            // 
+            // frmGrading
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(271, 99);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.txtMark);
+            this.Controls.Add(this.btnMark);
+            this.Controls.Add(this.lblInfo);
+            this.Name = "frmGrading";
+            this.Text = "Grading average";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.Button btnMark;
+        private System.Windows.Forms.TextBox txtMark;
+        private System.Windows.Forms.Label lblAnswer;
+    }
+}
+
---
+++ Grading/GradingForm.cs
@@ -0,0 +1,109 @@
+﻿// by: Andrew Mackinnon
+// Nov 3 2015
+// ICS3UR
+// Converts a letter mark to a percentage.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Grading
+{
+    public partial class frmGrading : Form
+    {
+        public string PercentageMaker(string letter)
+        {
+            // Converts a mark to a percentage
+
+            string mark = "That aint no mark";
+
+            if (letter == "4+")
+            {
+                mark = "Your percentage is: 95%";
+            }
+            else if (letter == "4")
+            {
+                mark = "Your percentage is: 90%";
+            }
+            else if (letter == "4-")
+            {
+                mark = "Your percentage is: 85%";
+            }
+            else if (letter == "3+")
+            {
+                mark = "Your percentage is: 77%";
+            }
+            else if (letter == "3")
+            {
+                mark = "Your percentage is: 75%";
+            }
+            else if (letter == "3-")
+            {
+                mark = "Your percentage is: 72%";
+            }
+            else if (letter == "2+")
+            {
+                mark = "Your percentage is: 67%";
+            }
+            else if (letter == "2")
+            {
+                mark = "Your percentage is: 65%";
+            }
+            else if (letter == "2-")
+            {
+                mark = "Your percentage is: 62%";
+            }
+            else if (letter == "1+")
+            {
+                mark = "Your percentage is: 57%";
+            }
+            else if (letter == "1")
+            {
+                mark = "Your percentage is: 55%";
+            }
+            else if (letter == "1-")
+            {
+                mark = "Your percentage is: 52%";
+            }
+            else if (letter == "R")
+            {
+                mark = "Your percentage is: 30%";
+            }
+            else if (letter == "NE")
+            {
+                mark = "Your percentage is: 0%";
+            }
+            else if (letter == "cool bro")
+            {
+                mark = "Your percentage is: hell ya";
+            }
+            
+            return mark;
+        }
+
+
+        public frmGrading()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMark_Click(object sender, EventArgs e)
+        {
+            // Varribles
+
+            string mark = this.txtMark.Text;
+            string percentage;
+            
+            // Process
+
+            percentage = PercentageMaker(mark);
+            this.lblAnswer.Text = percentage;
+        }
+    }
+}
